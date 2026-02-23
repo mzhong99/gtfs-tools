2b2967c74cb0ab2faa7dbfd7d15745041beb71ee
Matthew Zhong <matthewzhong@logmethods.com>
2026-02-22 21:54:50 -0500

    support ctrl+c

--- internal/web/gtfs_web/server.go
+++ internal/web/gtfs_web/server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -12,10 +13,8 @@ import (
 )
 
 type GtfsWebServer struct {
-	listenAddr string
-	db         *database.Database
-
-	router   chi.Router
+	db       *database.Database
+	server   *http.Server
 	renderer *Renderer
 }
 
@@ -35,23 +34,42 @@ func NewGtfsWebServer(ctx context.Context, listenAddr string, databaseUrl string
 	router.Use(middleware.Logger)
 	router.Use(middleware.Recoverer)
 
-	server := &GtfsWebServer{
-		db:         db,
-		listenAddr: listenAddr,
-		router:     router,
-		renderer:   renderer,
+	httpServer := &http.Server{
+		Addr:    listenAddr,
+		Handler: router,
 	}
 
-	server.router.Get("/", func(writer http.ResponseWriter, request *http.Request) {
+	server := &GtfsWebServer{
+		db:       db,
+		server:   httpServer,
+		renderer: renderer,
+	}
+
+	router.Get("/", func(writer http.ResponseWriter, request *http.Request) {
 		http.Redirect(writer, request, "/trains", http.StatusFound)
 	})
-	server.router.Get("/trains", server.handleTrainsPage)
-	server.router.Get("/trains/partial", server.handleTrainsPartial)
+	router.Get("/trains", server.handleTrainsPage)
+	router.Get("/trains/partial", server.handleTrainsPartial)
 
 	return server, nil
 }
 
+func (server *GtfsWebServer) startHosting() {
+	err := server.server.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		log.Fatalf("server error: %v", err)
+	}
+}
+
 func (server *GtfsWebServer) Serve(ctx context.Context) {
-	log.Printf("listening on http://localhost:%s", server.listenAddr)
-	log.Fatal(http.ListenAndServe(server.listenAddr, server.router))
+	log.Printf("listening on http://localhost:%s", server.server.Addr)
+
+	go server.startHosting()
+	<-ctx.Done()
+
+	log.Println("Shutting down.")
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	server.server.Shutdown(shutdownCtx)
 }
